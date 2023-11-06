961359af8c32220e03a29cf65101a2fbb61c6e7b
Jenii23 <jenifferal23972@hotmail.com>
2023-11-05 21:08:42 -0500

    #7 Capturar el evento submit del formulario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-
+const form = document.querySelector('#form');
+form.addEventListener('submit', function (e) {
+ e.preventDefault();
+ const inputWeight = e.target.querySelector('#weight');
+ const inputHeight = e.target.querySelector('#height');
+ const peso = Number(inputWeight.value);
+ const altura = Number(inputHeight.value);
+});
