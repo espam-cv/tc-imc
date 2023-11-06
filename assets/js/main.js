e87e34f65a5edf1c8eed98b897130f74fa7e171f
Jenii23 <33467235+Jenii23@users.noreply.github.com>
2023-11-05 21:19:13 -0500

    #7 Capturar el evento submit del formulario (#8)
    * #7 Capturar el evento submit del formulario
    
    * #7 Correción de los nombres de las constantes
    
    ---------
    
    Co-authored-by: Jenii23 <jenifferal23972@hotmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-
+const form = document.querySelector('#form');
+form.addEventListener('submit', function (e) {
+ e.preventDefault();
+ const inputWeight = e.target.querySelector('#weight');
+ const inputHeight = e.target.querySelector('#height');
+ const weight = Number(inputWeight.value);
+ const height = Number(inputHeight.value);
+});
