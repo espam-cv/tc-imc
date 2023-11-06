cb2bedb7d8383109109ae7dce4a51a7eb810417e
MarioJose952021 <78339108+MarioJose952021@users.noreply.github.com>
2023-11-05 21:56:19 -0500

    #9 Crear una función para obtener el nivel IMC (#10)
    * #9 Crear una función para obtener el nivel IMC
    
    * #9-Nombre de la variable cambiada a ingles

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,14 @@ form.addEventListener('submit', function (e) {
  const weight = Number(inputWeight.value);
  const height = Number(inputHeight.value);
 });
+
+function getLevelImc(imc) {
+  const level = ['Bajo peso', 'Peso normal', 'Sobrepeso',
+    'Obesidad grado 1', 'Obesidad grado 2', 'Obesidad grado 3'];
+  if (imc >= 39.9) return level[5];
+  if (imc >= 34.9) return level[4];
+  if (imc >= 29.9) return level[3];
+  if (imc >= 24.9) return level[2];
+  if (imc >= 18.5) return level[1];
+  if (imc < 18.5) return level[0];
+ }
